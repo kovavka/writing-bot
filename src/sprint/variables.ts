dd8d9415f33963386472e5db4740b83e60e804bf
Gin Kapger <gikapger@gmail.com>
2025-09-12 21:35:53 +0200

    Add more breaks

--- src/sprint/variables.ts
+++ src/sprint/variables.ts
@@ -1,4 +1,4 @@
 export const DEFAULT_BREAK_DURATION = 5
 export const LONGER_BREAK_DURATION = 15
 export const LUNCH_BREAK_DURATION = 30
-export const LUNCH_BREAK_AFTER = [6, 15]
+export const LUNCH_BREAK_AFTER = [6, 15, 24, 33, 42, 51, 60]
