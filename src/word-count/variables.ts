8537e71665d3fa59d5f2ec0d5a552fa7d494037c
Gin Kapger <gikapger@gmail.com>
2026-02-15 02:23:20 +0100

    Update marathon dates

--- src/word-count/variables.ts
+++ src/word-count/variables.ts
@@ -1,8 +1,8 @@
 import moment from 'moment-timezone'
 import { DATE_FORMAT, TIME_ZONE } from '../shared/variables'
 
-export const MARATHON_START_STR = '2025-11-01'
-export const MARATHON_END_STR = '2025-11-30'
+export const MARATHON_START_STR = '2025-02-14'
+export const MARATHON_END_STR = '2025-03-31'
 export const MARATHON_END_DATE = moment.tz(MARATHON_END_STR, DATE_FORMAT, TIME_ZONE)
 
 export const DEFAULT_PROJECT_NAME = 'Без названия'
